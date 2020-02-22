9446dc6d45456dd1446f75045aea0a9edf564036
Nerdy Deeds, LLC <nerdy.deeds@done.cheap>
2020-02-22 14:46:50 -0600

    master

--- extension.js
+++ extension.js
@@ -1,196 +1,64 @@
-/* // Waxing rheposodic about inlinement...
+/*
+// EXAMPLE USED IN GIF
+// Waxing rheposodic about inlinement...
 const little = (a,b) => a + Math.random() * (b - a); // Helper fn: Night at the Opera
 try {
-    assert(life.real);                      // Is this the real life?
-    assert(!life.real && life.fantasy);             // Is this just fantasy?
-}catch(landslide){                      // Caught in a landslide,
-    while((c = Array.from(/[^\\r\\e\\a\\l\\i\\t\\y]/gi))){    // No escape from reality.
-        [i1,i2] = c.filter((I,c)=>skies.__lookupGetter__((I.open()) && c)); // Open your eyes, look up to the skies and see
-        var self = {...'XY'}.hasOwnProperty(false) && !require("SYMPATHY"); // I'm just a poor boy, I need no sympathy,
-        do {                                                                    // Because...
-            !(easy=true) && self.insertAdjacentHTML('afterEnd', easy).remove(); // I'm easy come, easy go
-        } while(little(-1,1) < Infinity || little(-1,1) > -Infinity)                      // Little high, little low
-        var blow = windDirection => true;  // Any way the wind blows doesn't really matter to me, to me.
+    assert(life.real);                      // N - Is this the real life?
+    assert(!life.real && life.fantasy);             // E - Is this just fantasy?
+}catch(landslide){                      // R - Caught in a landslide,
+    while((c = Array.from(/[^\\r\\e\\a\\l\\i\\t\\y]/gi))){    // D - No escape from reality.
+        [i1,i2] = c.filter((I,c)=>skies.__lookupGetter__((I.open()) && c)); // Y - Open your eyes, look up to the skies and see
+        var self = {...'XY'}.hasOwnProperty(false) && !(require("SYMPATHY")); // D - I'm just a poor boy, I need no sympathy,
+        do {                                                                    // E - Because...
+            !(easy=true) && self.insertAdjacentHTML('afterEnd', easy).remove(); // E - I'm easy come, easy go
+        } while(little(-1,1) < Infinity || little(-1,1) > -Infinity)                      // D - Little high, little low
+        blow = windDirection => windDirection || !windDirection;  // S - Any way the wind blows doesn't really matter to me, to me.
     }
 }
- */
+*/
 
 const vscode = require('vscode');
 console.log('vscode :', vscode);
 function activate(context) {
-	
-	console.log ("EXTENSION - Inlinement (Aligns inline comments to the right of your code) - is now activ\e.")
-	let inlinement = vscode.commands.registerCommand('inlinement', function () {
-		console.log(this, vscode)
-		var  arrayOfSelected = []						// Array that will hold the lines selected within the editor
-			,lineByLineParse = []						// Constructed array that will hold all the lines in arrayOfSelected, post-parse
-			,commentSlashPos = 0						// Numeric value that will hold the max charPos of the //'s within the block
-			,backfillSpacing = '';						// String value that will hold the 
+    
+    console.log ("EXTENSION - Inlinement (Aligns inline comments to the right of your code) - is now listening for trigger keyboard shortcut: ⟦⌘⟧ + ⟦/⟧, ⟦⌘⟧ + ⟦/⟧")
+    let inlinement = vscode.commands.registerCommand('inlinement', function () {
+        const paddingSpaces  = new Array(128).fill(' ').join(' ');                          // Yields a 256-space string for the padding offset when aligning.
+        let activeTextEditor = vscode.window.activeTextEditor;                              // Active window's instance of VSCode
+        let activeDocument   = activeTextEditor.document;                                   // Active document (tab) within VSCode
+        let selection        = activeTextEditor.selection;                                  // Selected block of code, within the active document, within the active instance. That Jack built.
+        let startLine        = selection.start.line;                                        // Line number of the topmost line in the selection
+        let endLine          = selection.end.line;                                          // Line number of the bottommost line in the selection
+        let endLineLength    = activeDocument.lineAt(endLine).text.length                   // Number of chars in the bottommost line. Prevents user from having to select the whole line.
+        let replaceRange     = new vscode.Range(startLine, 0, endLine, endLineLength);      // VSCode Range (see their API) defining region within selection
+        let textToReplace    = activeDocument.getText(replaceRange);                        // Code contained within replaceRange
+        
+        const ltrim = (v) => v.replace(/^[\t\l\v ]/gi, '') || '';                           // HelperFN: trimes whitespace on the left side of the value, v, passed in
+        const rtrim = (v) => v.replace(/[\t\l\v ]$/gi, '') || '';                           // HelperFN: trimes whitespace on the right side of the value, v, passed in
 
-		let activeTextEditor = vscode.window.activeTextEditor;
-		let activeDocument = activeTextEditor.document;
-		
-		// 1. Obtain the selected region of test of the current window.
-		let selection = vscode.window.activeTextEditor.selection;
-		console.log('selection :', selection, vscode.window.activeTextEditor.selections);
-		
-		
-		// 1b. Break that out into start/end points.
-		let startLine = selection.start.line;																				// Gather first line in selection block...
-		let endLine = selection.end.line;																					//	... followed by the last.
-		let endLineLength = activeDocument.lineAt(endLine).text.length
-		let replaceRange = new vscode.Range(startLine, 0, endLine, endLineLength);
-  		console.log('replaceRange :', replaceRange);
+        const repositionInlineComments = (strInput, ...params) => {                         // The magic wot makes the pixies dance.
+            if(/\t/gim.test(strInput)) strInput = strInput.replace(/\t/gim, '    ');        // Normalize tabs to spaces. TODO: Convert it back to tabs if it started that way. I guess. If someone complains.
+            const inlineDelimiter = params.inlineDelimiter || "\\/{2}",                     // The inline-comment charater(s) that we'll be aligning by (regex)
+                  minColumnOffset = params.minColumnOffset || 20,                           // Any additional padding to tack on
+                  commentRE       = new RegExp('^([\\S\\t\\l\\v ]*?\\S{1})(?:[\\t\\l\\v ]*)' + inlineDelimiter + '([^/\\n\\r]+){1}$', 'gim'), // Construct the regex we'll need to perform the operation.
+                  maxColPos       = Math.max(...[...strInput.matchAll(commentRE)].map(v=>v[1].length)); // Find the longest pre-delimeter line
+            return strInput.replace(commentRE, (_x_, p1, p2)=>rtrim(p1) + (paddingSpaces).slice(0, (minColumnOffset + maxColPos-rtrim(p1).length)) + ' // '+ ltrim(p2)); // Add sufficient padding spaces to align each line.
+        }
 
-		
+        activeTextEditor.edit((TextEditorEdit) => {                                         // Knock the editor back into "edit" mode
+            TextEditorEdit.replace(replaceRange, repositionInlineComments(textToReplace));  // Replace the selected text with the now-comment-inined version
+        });
 
-		// 2. Iterate those lines, and gather the data
-		for(let i = startLine; i <= endLine; i++) arrayOfSelected.push(activeDocument.lineAt(i).text)						// Construct an array containing all the selected lines from the editor
-		arrayOfSelected.forEach((itrLineContents, itrLineLnNumber) => {                                                    // Iterate all lines in the selection
-			// Analyze each line within the selection for the values of both the comments and the code.
-			var itrLineDetailed = {                                                                                        // Create a JSON object to store metadata for each line 
-				itrLineContents:itrLineContents                                                                           // The textual contents of the line                                                             
-				,itrLineLnNumber:itrLineLnNumber                                                                           // The line's number
-				,hasInline: !/^(^(?:[\s\t ]*\/\/)).*$/.test(itrLineContents)                                               // Whether or not the line has an inline comment at all
-			}
-			console.log('itrLineDetailed :', itrLineDetailed);
-			if(itrLineDetailed.hasInline){                                                                                 // IF it proves it DOES (... have an inline)...
-				let activeLine = itrLineDetailed.itrLineContents;                                                          // ... alias the contents to stay DRY...
-				itrLineDetailed.commentPos = activeLine.lastIndexOf('//');                                                 // ... locate the last // in the line...
-				itrLineDetailed.linePrefix = activeLine.slice(0,itrLineDetailed.commentPos  ).replace(/[\t ]+$/, '');      // ... and break it into a suffix...
-				itrLineDetailed.lineSuffix = activeLine.slice(  itrLineDetailed.commentPos+2).replace(/^[\t ]+/, '');      // ...  and a prefix.
-			}else{                                                                                                         // OTHERWISE...
-				itrLineDetailed.linePrefix = itrLineDetailed.itrLineContents;                                              // ... grab the whole line as the prefix...
-				itrLineDetailed.lineSuffix = null;                                                                         // ... and null out the suffix.
-			}
-		
-			if(itrLineDetailed.commentPos > commentSlashPos) commentSlashPos = itrLineDetailed.commentPos;					// If the line being examined is the new largest, update our var.
-			lineByLineParse.push(itrLineDetailed);
-		})
-		
-		backfillSpacing = new Array(commentSlashPos).fill(' ').join('');
-		
-		lineByLineParse.forEach((itrLineContents, itrLineLnNumber) => {
-			if(!itrLineContents.hasInline || itrLineContents.linePrefix == null) return 
-			lineByLineParse[itrLineLnNumber].paddedText = lineByLineParse[itrLineLnNumber].linePrefix;
-			if(itrLineContents.lineSuffix != null) {
-				var revisedLineText  = lineByLineParse[itrLineLnNumber].paddedText;
-					revisedLineText  = (revisedLineText + backfillSpacing).slice(0, commentSlashPos);
-					revisedLineText += '// ' + lineByLineParse[itrLineLnNumber].lineSuffix;
-		
-				lineByLineParse[itrLineLnNumber].paddedText = revisedLineText
-			}
-		})
+    });
 
-		activeTextEditor.edit((TextEditorEdit) => {
-			TextEditorEdit.replace(replaceRange, lineByLineParse.flatMap(v=>v.paddedText).join('\n'));
-		});
-	
-	
-	});
-
-	context.subscriptions.push(inlinement);
+    context.subscriptions.push(inlinement);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {}
+exports.deactivate = deactivate;
 
 module.exports = {
-	activate,
-	deactivate
+    activate,
+    deactivate
 }
-
-
-
-
-
-
-
-
-
-// const vscode = require('vscode');
-// const VSC_ECA_spaces = new Array(101).join(' ');                // Creates a constant, global variable that's simply a string comprised of 100 spaces
-// const addBlankString = n => VSC_ECA_spaces.slice(-n);           // Returns the last n characters of said string (which coincidentally is the # requested)
-
-// function activate(context) {
-
-// 	// Use the console to output diagnostic information (console.log) and errors (console.error)
-// 	// This line of code will only be executed once when your extension is activated
-// 	// console.log('Congratulations, your extension "helloworld" is now active!');
-
-// 	// The command has been defined in the package.json file
-// 	// Now provide the implementation of the command with  registerCommand
-// 	// The commandId parameter must match the command field in package.json
-// 	let disposable = vscode.commands.registerCommand('extension.commentaligner', function () {
-// 		// The code you place here will be executed every time your command is executed
-
-// 		// Process
-// 		// 1a. Obtain the selected region of test of the current window.
-// 		// 1b. Break that out into start/end points.
-// 		// 2.  Iterate those lines, and gather the data
-// 		// 3. 获取非注释内容最长行
-// 		// 4. 归位其他行注释位置，对齐最长行注释位置
-
-// 		let activeTextEditor = vscode.window.activeTextEditor;
-// 		let activeDocument = activeTextEditor.document;
-
-// 		// 1. Obtain the selected region of test of the current window.
-// 		let selection = vscode.window.activeTextEditor.selection;
-
-// 		// 1b. Break that out into start/end points.
-// 		let startLine = selection.start.line;
-// 		let endLine = selection.end.line;
-
-// 		// 2. Iterate those lines, and gather the data
-// 		let commentArr = [];                                                           // 选中行信息缓存数组
-// 		let commentIndexArr = [];                                                      // 选中行注释起始脚标数组，用于筛选出非注释文本内容最长行
-// 		for(let i = startLine; i <= endLine; i++) {
-// 			let curLineText = activeDocument.lineAt(i).text;                           // 当前行文本内容
-// 			let curLineCommentIndex = curLineText.lastIndexOf('//');                   // 注释起始脚标，行的最后标记符号；使用时应当避免含有注释符号字符串的非注释行
-// 			let curLineComment = curLineText.slice(curLineCommentIndex);               // 注释文本
-// 			let curLineTextWithOutComment = curLineText.slice(0, curLineCommentIndex); // 非注释文本
-
-// 			commentIndexArr.push(curLineCommentIndex);
-// 			commentArr.push({
-// 				line: i,
-// 				lineLength: curLineText.length,
-// 				curLineText: curLineText,
-// 				curLineTextWithOutComment: curLineTextWithOutComment,
-// 				commentIndex: curLineCommentIndex,
-// 				comment: curLineComment
-// 			});
-// 		}
-		
-// 		// 3. 获取非注释文本内容最长行
-// 		let maxCommentLengthIndex = Math.max.apply(null, commentIndexArr);
-		
-// 		// 4. 归位其他行注释位置，对齐最长行注释位置
-// 		let newText = commentArr.map(item => {
-// 			if (item.commentIndex === -1) {
-// 				return item.lineLength === 0 ? '' : item.curLineText;
-// 			}
-// 			return item.lineLength === 0 ? '' : item.curLineTextWithOutComment + addBlankString(maxCommentLengthIndex - item.commentIndex) + item.comment;
-// 		});
-// 		let replaceRange = new vscode.Range(startLine, 0, endLine, commentArr[commentArr.length - 1].lineLength);
-
-// 		// 调用编辑接口
-// 		activeTextEditor.edit((TextEditorEdit) => {
-// 			TextEditorEdit.replace(replaceRange, newText.join('\n'));
-// 		});
-
-// 		// Display a message box to the user
-// 		// vscode.window.showInformationMessage('success!');
-// 	});
-
-// 	context.subscriptions.push(disposable);
-// }
-// exports.activate = activate;
-
-// // this method is called when your extension is deactivated
-// function deactivate() {}
-
-// module.exports = {
-// 	activate,
-// 	deactivate
-// }
